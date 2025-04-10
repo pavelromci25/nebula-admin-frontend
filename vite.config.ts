124f348dc8caf6cf15cb84bb12e0b1ca33de8b67
pavelromci25 <pavel.romci25@gmail.com>
2025-04-10 12:44:27 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nebula-admin-frontend/', // Должно совпадать с именем репозитория
+  base: '/', // Устанавливаем base в корень, так как Vercel будет хостить проект по корневому пути
+  build: {
+    outDir: 'dist', // Указываем папку для сборки
+    assetsDir: 'assets' // Указываем папку для ресурсов (по умолчанию 'assets')
+  }
 });
